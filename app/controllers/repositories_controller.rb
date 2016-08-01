e939d021c1e48d285a5e5d66cc7d43c76b56690e
lastromanticx <giladbarkan@live.com>
2016-08-01 16:51:20 -0400

    got username and token

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,21 @@
 class RepositoriesController < ApplicationController
+  before_action :authenticate_user
+
   def index
+    # get github user info
+    resp = Faraday.get("https://api.github.com/user") do |req|
+      req.params['access_token'] = session[:token]
+    end
+ 
+    body = JSON.parse(resp.body)
+    @github_username = body["login"]
+
+    #get repos
+    resp = Faraday.get("https://api.github.com/users/#{@github_username}/repos") do |req|
+      req.params['access_token'] = session[:token]
+    end
+
+    @repos = JSON.parse(resp.body)
   end
 
   def create
