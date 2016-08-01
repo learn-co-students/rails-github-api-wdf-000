5e645907f6e04db61632cd1c9deb0fea3619dc01
lastromanticx <giladbarkan@live.com>
2016-08-01 17:23:53 -0400

    does all three assignments

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -19,5 +19,12 @@ class RepositoriesController < ApplicationController
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params['access_token'] = session[:token]
+      req.body = "{ \"name\": \"#{params[:name]}\" }"
+    end
+
+    redirect_to root_path
   end
 end
