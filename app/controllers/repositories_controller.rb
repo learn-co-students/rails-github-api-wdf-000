5698757d2b98e91ef179e47d1ed0c78b4ae46652
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-17 22:34:13 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,23 @@
 class RepositoriesController < ApplicationController
   def index
+    # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    resp = Faraday.get("https://api.github.com/user?access_token=#{session[:token]}")
+    if resp.success?
+      @username = JSON.parse(resp.body)['login']
+      repourl = JSON.parse(resp.body)['repos_url']
+      @repos = JSON.parse(Faraday.get(repourl).body)
+    end
   end
 
   def create
+    resp = Faraday.post('https://api.github.com/user/repos') do |req|
+      req.params["access_token"] = session[:token]
+      req.body = {name: params[:name]}.to_json
+    end
+    # response = Faraday.post('https://api.github.com/user/repos') do |req|
+    #    req.body = {name: params[:name]}.to_json
+    #    req.headers = {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    #  end
+    redirect_to root_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.post "https://github.com/login/oauth/access_token" do | rep |
+      rep.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      rep.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      rep.params['redirect_uri'] = "http://localhost:3000/auth"
+      rep.params['code'] = params[:code]
+    end
+    session[:token] = resp.body.match(/=(\w+)/)[1]
+    redirect_to root_path
   end
+
 end
