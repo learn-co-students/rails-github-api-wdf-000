988a194af36018eb066b29105a16f2e97563ad5e
satub <satu.barnhill@gmail.com>
2016-10-11 14:04:19 -0400

    Webpage works, tests do not

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,26 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get 'https://api.github.com/user/repos' do |req|
+        # req.params['client_id'] = ENV['GITHUB_ID']
+        # req.params['client_secret'] = ENV['GITHUB_SECRET']
+        req.params['v'] = 'application/vnd.github.v3+json'
+        req.params['user'] = session[:username]
+        req.params['access_token'] = session[:token]
+        req.options.timeout = 5 ###This is in seconds, I think...
+      end
+      @repos  = JSON.parse(resp.body)
+
+      # binding.pry
   end
 
   def create
+    resp = Faraday.post('https://api.github.com/user/repos') do |req|
+      req.body = { name: params[:name]}.to_json
+      req.params['access_token'] = session[:token]
+      req.params['v'] = 'application/vnd.github.v3+json'
+   end
+  #  binding.pry
+   redirect_to root_path
+
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['GITHUB_ID']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      # req.params['v'] = 'application/vnd.github.v3+json'
+      # req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://localhost:8888/auth"
+      req.params['code'] = params[:code]
+    end
+
+    # binding.pry
+    # body = JSON.parse(resp.body)
+    session[:token] = resp.body.match(/\=(\w*)\&/).captures[0]
+
+    username = Faraday.get("https://api.github.com/user") do |req|
+      req.params['access_token'] = resp.body.match(/\=(\w*)\&/).captures[0]
+    end
+    # binding.pry
+    session[:username] = JSON.parse(username.body)["login"]
+    redirect_to root_path
   end
 end
