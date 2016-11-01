14688608c16e38ef5ed0ebb91e598188cb343afa
fidelscodes <fidelscodes@gmail.com>
2016-11-01 16:49:56 -0400

    Done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,25 @@
 class RepositoriesController < ApplicationController
   def index
+    # get current user using session[:token]
+    resp = Faraday.get("https://api.github.com/user") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    @user = JSON.parse(resp.body)
+
+    # get current user repos
+    resp2 = Faraday.get(@user['repos_url'])
+
+    @repos = JSON.parse(resp2.body)
   end
 
   def create
+    repo = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Content-Type'] = "application/json"
+      req.params['access_token'] = session[:token]
+      req.body = {
+        "name": "#{params[:name]}"
+      }.to_json
+    end
+    redirect_to root_path
   end
 end
