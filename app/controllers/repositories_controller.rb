263bf2da07b0f02fe02197fb694ff0874fbdc0e9
ozPop <ozziepopovas@gmail.com>
2016-11-01 14:25:32 -0400

    add displaying of name

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,10 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get('https://api.github.com/user') do |req|
+      req.params['access_token'] = session[:token]
+    end
+    body = JSON.parse(resp.body)
+    @username = body['login']
   end
 
   def create
