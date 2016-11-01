996a376d21a81bd4a04e5de678cd9ec504443887
ozPop <ozziepopovas@gmail.com>
2016-11-01 16:58:10 -0400

    update controllers

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,5 +8,14 @@ class RepositoriesController < ApplicationController
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params['access_token'] = session[:token]
+      req.body = {
+        "name" => "#{params[:name]}"
+      }.to_json
+    end
+    binding.pry
+    redirect_to root_path
   end
 end
