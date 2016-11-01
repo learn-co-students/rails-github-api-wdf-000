c6456f84b07a8598f9a98072151392b520bf463a
fidelscodes <fidelscodes@gmail.com>
2016-11-01 17:04:46 -0400

    Update solution

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,5 +13,11 @@ class SessionsController < ApplicationController
   body = JSON.parse(resp.body)
   session[:token] = body["access_token"]
   redirect_to root_path
+
+  # another way to sign in the user. This would be better because then we would
+  # have access to session[:username] throughout the app
+  # user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  #   user_json = JSON.parse(user_response.body)
+  #   session[:username] = user_json["login"]
   end
 end
